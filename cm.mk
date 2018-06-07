dd8542165f4e6d06820b8665bcc8aa832998db1f
Isaac Chen <isaacchen@isaacchen.cn>
2018-06-07 21:23:50 +0800

    pa24: Initial import
     * From zuk ham
    
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

---
+++ cm.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from ham device
+$(call inherit-product, device/sharp/pa24/pa24.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := cm_pa24
+PRODUCT_DEVICE := pa24
+PRODUCT_MANUFACTURER := Sharp
+PRODUCT_MODEL := 304SH
+
+PRODUCT_GMS_CLIENTID_BASE := android-sharp
+
+PRODUCT_BRAND := Sharp
+TARGET_VENDOR := sharp
+TARGET_VENDOR_PRODUCT_NAME := pa24
+TARGET_VENDOR_DEVICE_NAME := 304SH
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=304SH PRODUCT_NAME=pa24
