8aa3f12cea157bb589e7337668bfe04fc6b81bbf
JerryCheng <501950452@qq.com>
2018-08-05 04:46:27 +0800

    pa26: BoardConfig: remove Wifi - EAP-SIM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,10 +122,6 @@ TARGET_USES_WCNSS_CTRL           := true
 TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 
-# Wifi - EAP-SIM
-CONFIG_EAP_PROXY                 := qmi
-CONFIG_EAP_PROXY_DUAL_SIM        := false
-
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
